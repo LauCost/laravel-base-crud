8ca3fa2e4a9b57631a1deca228e1d98c5bc76c57
Laurentiu <lcostin40@gmail.com>
2022-01-19 15:48:27 +0100

    Aggiunto cartella img e partials

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,25 @@
+<header>
+
+    <div class="cont_img_nav">
+
+        <div class="cont_img_header">
+
+            <img src=" {{ asset('images/dc-logo.png') }} " alt="" />
+        </div>
+
+        <nav>
+            <a href="#">CHARACTERS</a>
+            <a class="attivo" href="#">COMICS</a>
+            <a href="#">MOVIES</a>
+            <a href="#">TV</a>
+            <a href="#">GAMES</a>
+            <a href="#">COLLECTIBLES</a>
+            <a href="#">VIDEOS</a>
+            <a href="#">FANS</a>
+            <a href="#">NEWS</a>
+            <a href="#">SHOP</a>
+        </nav>
+
+    </div>
+
+</header>
